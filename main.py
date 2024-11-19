1f94b5b93109889ec30f5854491a7e07b19801f5
stefantwoods <stefan5365@hotmail.com>
2024-11-18 22:24:13 -0700

    screen with character

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import pygame
 from constants import *
+from player import player
 
 def main():
-    pygame.init
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH/2
+    y = SCREEN_HEIGHT/2
+    player1 = player(x,y)
 
     while True:
         for event in pygame.event.get():
@@ -13,8 +20,13 @@ def main():
         # Make the entire screen black
         screen.fill((0,0,0))
 
+        # Draw the player
+        player1.draw(screen)
+
         # Update the screen
         pygame.display.flip()
+
+        dt = clock.tick(60)
 
 if __name__ == "__main__":
     main()
